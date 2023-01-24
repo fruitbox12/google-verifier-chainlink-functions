eb9962ae5d543d3861298e4349f4e5e6ebca72a8
polarzero <0xpolarzero@gmail.com>
2023-01-24 19:46:51 +0100

    Add hardhat project to API before adaptations

---
+++ implementation/frontend/pages/api/systems/helper-hardhat-config.js
@@ -0,0 +1,16 @@
+const developmentChains = ['hardhat', 'localhost'];
+
+const TWITTER_VERIFIER = '0xdb4c4c35E9B59a48E9931476753f5A183cD7280d';
+const SUB_ID = 20;
+
+// Username & address to test
+const TWITTER_USERNAME = 'testAcc09617400';
+const ETHEREUM_ADDRESS = '0xc06d127e504a944f63bc750d8f512556c576f3ef';
+
+module.exports = {
+  developmentChains,
+  TWITTER_VERIFIER,
+  SUB_ID,
+  TWITTER_USERNAME,
+  ETHEREUM_ADDRESS,
+};
