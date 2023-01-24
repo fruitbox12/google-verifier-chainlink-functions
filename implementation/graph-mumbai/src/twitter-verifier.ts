8cecf931708117f217b2e9c2d9299ce43bf9855d
polarzero <0xpolarzero@gmail.com>
2023-01-24 12:21:34 +0100

    Update entity name

--- implementation/graph-mumbai/src/twitter-verifier.ts
+++ implementation/graph-mumbai/src/twitter-verifier.ts
@@ -1,37 +1,37 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from '@graphprotocol/graph-ts';
 import {
   TwitterVerifier,
   OCRResponse,
   OwnershipTransferRequested,
   OwnershipTransferred,
   RequestFulfilled,
-  RequestSent
-} from "../generated/TwitterVerifier/TwitterVerifier"
-import { ExampleEntity } from "../generated/schema"
+  RequestSent,
+} from '../generated/TwitterVerifier/TwitterVerifier';
+import { TwitterVerificationRequest } from '../generated/schema';
 
 export function handleOCRResponse(event: OCRResponse): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = TwitterVerificationRequest.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new TwitterVerificationRequest(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
-  entity.requestId = event.params.requestId
-  entity.result = event.params.result
+  entity.requestId = event.params.requestId;
+  entity.result = event.params.result;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -58,7 +58,7 @@ export function handleOCRResponse(event: OCRResponse): void {
 }
 
 export function handleOwnershipTransferRequested(
-  event: OwnershipTransferRequested
+  event: OwnershipTransferRequested,
 ): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
