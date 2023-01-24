3518a6c7a80571fd1cbf948ec847917516408f83
polarzero <0xpolarzero@gmail.com>
2023-01-24 15:48:41 +0100

    Update table with columns & data from graphQL query

--- implementation/frontend/components/VerifiedTable/VerifiedTable.jsx
+++ implementation/frontend/components/VerifiedTable/VerifiedTable.jsx
@@ -1,9 +1,42 @@
+import { Table } from 'antd';
 import { useEffect } from 'react';
+import { Address, ElapsedTime, TwitterUsername } from '../Utils';
 import stores from '../../stores';
 
-// For the columns, there will be the timestamp, address, username and success
-// of the verification. The table will be sorted by timestamp, with the most
-// recent at the top.
+const columns = [
+  {
+    title: 'Date',
+    dataIndex: 'timestamp',
+    key: 'date',
+    render: (timestamp) => <ElapsedTime timestamp={timestamp} />,
+  },
+  {
+    title: 'Ethereum address',
+    dataIndex: 'address',
+    key: 'address',
+    render: (address) => <Address address={address} />,
+  },
+  {
+    title: 'Twitter username',
+    dataIndex: 'username',
+    key: 'username',
+    render: (username) => <TwitterUsername username={username} />,
+  },
+  {
+    title: 'Verification',
+    dataIndex: 'result',
+    key: 'result',
+    render: (result) => {
+      if (Number(result) === -1) {
+        return <span className='status error'>Error</span>;
+      } else if (Number(result) === 0) {
+        return <span className='status failed'>Failed</span>;
+      } else if (Number(result) === 1) {
+        return <span className='status success'>Success</span>;
+      }
+    },
+  },
+];
 
 const VerifiedTable = () => {
   const { data, loaded, setData } = stores.useData();
@@ -28,6 +61,11 @@ const VerifiedTable = () => {
         For testing purpose, you can verify an account multiple times, which
         will however not create multiple entries in the table.
       </span>
+      <Table
+        columns={columns}
+        dataSource={data.map((entry) => entry.data)}
+        rowKey={(entry) => entry.timestamp}
+      />
     </div>
   );
 };
