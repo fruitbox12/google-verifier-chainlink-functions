d5b1f5c45d1c83b8283ff7330c222693c04f1329
polarzero <0xpolarzero@gmail.com>
2023-01-24 16:12:25 +0100

    Add search, filter and sorting to table

--- implementation/frontend/components/VerifiedTable/VerifiedTable.jsx
+++ implementation/frontend/components/VerifiedTable/VerifiedTable.jsx
@@ -1,55 +1,61 @@
 import { Table } from 'antd';
 import { useEffect } from 'react';
 import { Address, ElapsedTime, TwitterUsername } from '../Utils';
+import { useSearch } from '../../hooks';
 import stores from '../../stores';
-
-const columns = [
-  {
-    title: 'Date',
-    dataIndex: 'timestamp',
-    key: 'date',
-    render: (timestamp) => <ElapsedTime timestamp={timestamp} />,
-  },
-  {
-    title: 'Ethereum address',
-    dataIndex: 'address',
-    key: 'address',
-    render: (address) => <Address address={address} />,
-  },
-  {
-    title: 'Twitter username',
-    dataIndex: 'username',
-    key: 'username',
-    render: (username) => <TwitterUsername username={username} />,
-  },
-  {
-    title: 'Verification',
-    dataIndex: 'result',
-    key: 'result',
-    render: (result) => {
-      if (Number(result) === -1) {
-        return <span className='status error'>Error</span>;
-      } else if (Number(result) === 0) {
-        return <span className='status failed'>Failed</span>;
-      } else if (Number(result) === 1) {
-        return <span className='status success'>Success</span>;
-      }
-    },
-  },
-];
 
 const VerifiedTable = () => {
   const { data, loaded, setData } = stores.useData();
+  const { getColumnSearchProps } = useSearch();
+
+  const columns = [
+    {
+      title: 'Date',
+      dataIndex: 'timestamp',
+      key: 'date',
+      render: (timestamp) => <ElapsedTime timestamp={timestamp} />,
+      sorter: (a, b) => a.timestamp - b.timestamp,
+      sortDirections: ['descend', 'ascend'],
+    },
+    {
+      title: 'Ethereum address',
+      dataIndex: 'address',
+      key: 'address',
+      ...getColumnSearchProps('address'),
+      render: (address) => <Address address={address} />,
+    },
+    {
+      title: 'Twitter username',
+      dataIndex: 'username',
+      key: 'username',
+      ...getColumnSearchProps('username'),
+      render: (username) => <TwitterUsername username={username} />,
+    },
+    {
+      title: 'Verification',
+      dataIndex: 'result',
+      key: 'result',
+      render: (result) => {
+        if (Number(result) === -1) {
+          return <span className='status error'>Error</span>;
+        } else if (Number(result) === 0) {
+          return <span className='status failed'>Failed</span>;
+        } else if (Number(result) === 1) {
+          return <span className='status success'>Success</span>;
+        }
+      },
+      filters: [
+        { text: 'Error', value: -1 },
+        { text: 'Failed', value: 0 },
+        { text: 'Success', value: 1 },
+      ],
+      onFilter: (value, record) => Number(record.result) === value,
+    },
+  ];
 
   useEffect(() => {
     setData();
   }, []);
-
-  useEffect(() => {
-    if (data) {
-      console.log(data);
-    }
-  }, [data]);
 
   if (!loaded || !data) {
     return 'loading';
@@ -59,7 +65,7 @@ const VerifiedTable = () => {
     <div className='verified-table'>
       <span>
         For testing purpose, you can verify an account multiple times, which
-        will however not create multiple entries in the table.
+        will create multiple entries in the table.
       </span>
       <Table
         columns={columns}
---
+++ implementation/frontend/hooks/useSearch.js
@@ -0,0 +1,80 @@
+import { useRef, useState } from 'react';
+import { Button, Input, Space } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
+const useSearch = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size='small'
+            style={{ width: 100 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size='small'
+            style={{ width: 100 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : '',
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current.select());
+      }
+    },
+  });
+
+  return { getColumnSearchProps };
+};
+
+export default useSearch;
