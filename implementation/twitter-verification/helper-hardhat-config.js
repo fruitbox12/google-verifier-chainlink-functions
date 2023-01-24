bb1d28f66c6b50d4fdea680d78642b7bebb4b135
polarzero <0xpolarzero@gmail.com>
2023-01-24 17:02:35 +0100

    Update with new verifier address

--- implementation/twitter-verification/helper-hardhat-config.js
+++ implementation/twitter-verification/helper-hardhat-config.js
@@ -1,6 +1,6 @@
 const developmentChains = ['hardhat', 'localhost'];
 
-const TWITTER_VERIFIER = '0xbE40a3f619C855A1C059f3b684398703cd51609d';
+const TWITTER_VERIFIER = '0xdb4c4c35E9B59a48E9931476753f5A183cD7280d';
 const SUB_ID = 18;
 
 // Username & address to test
