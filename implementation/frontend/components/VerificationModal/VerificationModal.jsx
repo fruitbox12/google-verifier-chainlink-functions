dcea56598ee70f9bc23110fa6accecb0107e3a1f
polarzero <0xpolarzero@gmail.com>
2023-01-24 19:16:07 +0100

    Add structure for request

--- implementation/frontend/components/VerificationModal/VerificationModal.jsx
+++ implementation/frontend/components/VerificationModal/VerificationModal.jsx
@@ -1,8 +1,29 @@
 import { Modal } from 'antd';
+import { useAccount } from 'wagmi';
+import { useEncryption } from '../../hooks';
 import stores from '../../stores';
 
 const VerificationModal = () => {
   const { isModalOpen, setIsModalOpen } = stores.useVerification();
+  const { encrypt } = useEncryption();
+  const { address } = useAccount();
+  const username = 'wagmi';
+
+  const requestVerification = async () => {
+    // Encrypt username & address
+    const encrypted = encrypt(username, address);
+
+    const res = await fetch('/api/verify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, address, encrypted }),
+    });
+    const data = await res.json();
+
+    // TODO Handle error, maybe the decryption failed: try again
+  };
 
   return (
     <Modal
