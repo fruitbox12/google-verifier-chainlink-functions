b8eaee7d9da5408a821cd527a3e2ffab20f34b2a
polarzero <0xpolarzero@gmail.com>
2023-01-25 15:42:17 +0100

    Add isRequesting for loading

--- implementation/frontend/stores/useVerification.js
+++ implementation/frontend/stores/useVerification.js
@@ -3,4 +3,7 @@ import { create } from 'zustand';
 export default create((set) => ({
   isModalOpen: false,
   setIsModalOpen: (isOpen) => set({ isModalOpen: isOpen }),
+
+  isRequesting: false,
+  setIsRequesting: (isRequesting) => set({ isRequesting }),
 }));
