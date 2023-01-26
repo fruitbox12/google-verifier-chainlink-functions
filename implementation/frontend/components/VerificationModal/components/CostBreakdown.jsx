c31262438348488d0315228d5959178c82b3d195
polarzero <0xpolarzero@gmail.com>
2023-01-26 15:52:09 +0100

    Implement cost table

--- implementation/frontend/components/VerificationModal/components/CostBreakdown.jsx
+++ implementation/frontend/components/VerificationModal/components/CostBreakdown.jsx
@@ -1,13 +1,66 @@
+import { Divider, Table } from 'antd';
+import { useState } from 'react';
+import { useCMK } from '../../../hooks';
+import { RoundedCurrency } from '../../Utils';
+
+const columns = [
+  {
+    title: 'Transmission cost',
+    dataIndex: 'transmissionCost',
+    key: 'transmissionCost',
+  },
+  {
+    title: 'Base fee',
+    dataIndex: 'baseFee',
+    key: 'baseFee',
+  },
+  {
+    title: 'Total cost',
+    dataIndex: 'totalCost',
+    key: 'totalCost',
+  },
+];
+
 const CostBreakdown = ({ billing }) => {
+  // We just need an estimate so we can make a regular unprotected API call
+  // to get the price in USD
+  const { priceInDollar } = useCMK('LINK');
+
+  const billingFormatted = {
+    transmissionCost: (
+      <RoundedCurrency value={billing.transmissionCost} currency='LINK' />
+    ),
+    baseFee: <RoundedCurrency value={billing.baseFee} currency='LINK' />,
+    totalCost: (
+      <span style={{ fontWeight: 600 }}>
+        <RoundedCurrency value={billing.totalCost} currency='LINK' />{' '}
+        {priceInDollar && (
+          <span style={{ fontSize: '1rem' }}>
+            <Divider type='vertical' />
+            <span
+              style={{ /* fontSize: '1rem',  */ color: 'var(--color-blue)' }}
+            >
+              <RoundedCurrency
+                value={Number(billing.totalCost) * Number(priceInDollar)}
+                currency='$'
+              />
+            </span>
+          </span>
+        )}
+      </span>
+    ),
+  };
+
   return (
     <div className='cost-breakdown'>
-      {billing &&
-        Object.keys(billing).map((key) => (
-          <div className='cost-breakdown-item' key={key}>
-            <span className='label'>{key}: </span>
-            <span className='value'>{billing[key]}</span>
-          </div>
-        ))}
+      {billing && (
+        <Table
+          columns={columns}
+          dataSource={[billingFormatted]}
+          rowKey='totalCost'
+          pagination={false}
+        />
+      )}
     </div>
   );
 };
