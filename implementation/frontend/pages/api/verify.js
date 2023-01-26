e34d4185e83db462c9b02c88e0f234049c8a8016
polarzero <0xpolarzero@gmail.com>
2023-01-26 22:04:36 +0100

    require -> import + update spreadsheet with cost

--- implementation/frontend/pages/api/verify.js
+++ implementation/frontend/pages/api/verify.js
@@ -1,10 +1,10 @@
-const executeRequest = require('./systems/scripts/execute-request');
-const { useEncryption } = require('../../hooks');
+import executeRequest from './systems/scripts/execute-request';
+import { useEncryption } from '../../hooks';
 const { decrypt, testMatch } = useEncryption();
 
 const handler = async (req, res) => {
   const { body } = req;
-  const { username, address, encrypted } = body;
+  const { username, address, encrypted, baseUrl } = body;
 
   try {
     const decrypted = decrypt(encrypted);
@@ -15,9 +15,49 @@ const handler = async (req, res) => {
     }
 
     const result = await executeRequest(username, address, 'mumbai');
+
+    // Update the spreadsheet with the new data
+    const updateSuccess = await updateSpreadsheetCost(result, baseUrl);
+    // We don't really need that success value, but it's here anyway
+
     return res.status(200).json(result);
   } catch (err) {
     return res.status(500).json({ error: err.message });
+  }
+};
+
+const updateSpreadsheetCost = async (result, baseUrl) => {
+  console.log(result);
+  if (!result) return false;
+  if (!result.billing) return false;
+
+  try {
+    const response = await fetch(
+      `${baseUrl}/api/sheets/write-spreadsheet-cost`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...result.billing,
+          requestId: result.requestId,
+          errorMsg: result.errorMsg,
+        }),
+      },
+    );
+    const data = await response.json();
+    console.log(data);
+
+    const success = data.data.updates.updatedRows === 1;
+    if (!success) {
+      return false;
+    }
+
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
   }
 };
 
