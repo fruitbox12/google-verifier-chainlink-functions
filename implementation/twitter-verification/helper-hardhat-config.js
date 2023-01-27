10140bee1157415f3cd7f0ede4689cf30c3c4803
polarzero <0xpolarzero@gmail.com>
2023-01-27 21:26:49 +0100

    Update with new contract address & subid

--- implementation/twitter-verification/helper-hardhat-config.js
+++ implementation/twitter-verification/helper-hardhat-config.js
@@ -1,7 +1,7 @@
 const developmentChains = ['hardhat', 'localhost'];
 
-const TWITTER_VERIFIER = '0xD4168950d1529265A6c96311Ec8a622CdBD2610B';
-const SUB_ID = 26;
+const TWITTER_VERIFIER = '0x690a16033837fEd6BC078340c4fdE08Df5741829';
+const SUB_ID = 30;
 
 // Username & address to test
 const TWITTER_USERNAME = 'testAcc09617400';
