17f89ccc5046d635187fe39697f49f26ca97d49a
polarzero <0xpolarzero@gmail.com>
2023-01-27 21:25:49 +0100

    Take care of NaN

--- implementation/frontend/components/Utils/RoundedCurrency.jsx
+++ implementation/frontend/components/Utils/RoundedCurrency.jsx
@@ -2,11 +2,12 @@ import { Tooltip } from 'antd';
 
 const RoundedCurrency = ({ value, currency }) => {
   const rounded = () => {
+    if (isNaN(value) || Number(value) === 0) return 0;
     // If > 1 LINK, round to 2 decimal places
-    if (value >= 1) {
+    if (Number(value) >= 1) {
       return Number(Number(value).toFixed(2));
     }
-
+    console.log(value);
     const trailingZeros = value.toString().split('.')[1].match(/0+/)[0].length;
     return Number(Number(value).toFixed(trailingZeros + 3));
   };
