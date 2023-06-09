f4b03ef4baa4cb8bb887656c1789bdd391743429
fruitbox12 <dylanwong007@gmail.com>
2023-06-08 21:03:17 -0700

    Update helper-hardhat-config.js

--- implementation/twitter-verification/helper-hardhat-config.js
+++ implementation/twitter-verification/helper-hardhat-config.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 // Function to read the deployment result JSON file
 function readDeploymentResult(filePath) {
+    let address;
     try {
         const data = fs.readFileSync(filePath, 'utf8');
         if (!data) {
@@ -10,10 +11,26 @@ function readDeploymentResult(filePath) {
             return;
         }
         const result = JSON.parse(data);
-        return result;
+        address = result.address;  // Accessing the 'address' field
     } catch (error) {
         console.error('An error occurred:', error);
     }
+    return address;  // Return 'address' instead of the whole JSON object
+}
+
+// Then call the function with your file path
+const deploymentAddress = readDeploymentResult('./deployments/mumbai/TwitterVerifier.json');
+
+if (deploymentAddress) {
+    try {
+    console.log(`Deployment address: ${deploymentAddress}`);
+    }
+    catch(error) {
+            console.log('Address could not be retrieved');
+
+    }
+} else {
+    console.log('Address could not be retrieved');
 }
 
 // then call the function with your file path
@@ -23,12 +40,12 @@ const deploymentResult = readDeploymentResult('./deployments/mumbai/TwitterVerif
 // Function to get the deployed contract's address
 const getDeployedContractAddress = () => {
   const deploymentResult = deploymentResult
-  const contractName = 'YourContract'; // Replace with your contract name
+  const contractName = 'TwitterVerifier'; // Replace with your contract name
   const network = 'mumbai'; // Replace with your network name
   const deployment = deploymentResult[contractName][network];
   return deployment.address;
 };
-const TWITTER_VERIFIER = '';
+const TWITTER_VERIFIER = deploymentResult;
 const SUB_ID = 30;
 
 // Username & address to test
