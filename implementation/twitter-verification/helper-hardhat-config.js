592ce96f629ee798c6b5b208db0d31d76f792cb5
fruitbox12 <dylanwong007@gmail.com>
2023-06-08 20:31:39 -0700

    Update helper-hardhat-config.js

--- implementation/twitter-verification/helper-hardhat-config.js
+++ implementation/twitter-verification/helper-hardhat-config.js
@@ -1,5 +1,20 @@
 const developmentChains = ['hardhat', 'localhost'];
+const fs = require('fs');
 
+// Function to read the deployment result JSON file
+const readDeploymentResult = () => {
+  const deploymentResult = fs.readFileSync('./deployments/your-deployment-result.json');
+  return JSON.parse(deploymentResult);
+};
+
+// Function to get the deployed contract's address
+const getDeployedContractAddress = () => {
+  const deploymentResult = readDeploymentResult();
+  const contractName = 'YourContract'; // Replace with your contract name
+  const network = 'mumbai'; // Replace with your network name
+  const deployment = deploymentResult[contractName][network];
+  return deployment.address;
+};
 const TWITTER_VERIFIER = '';
 const SUB_ID = 30;
 
@@ -13,4 +28,12 @@ module.exports = {
   SUB_ID,
   TWITTER_USERNAME,
   ETHEREUM_ADDRESS,
+   namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    contractAddress: {
+      default: getDeployedContractAddress(),
+    },
+  },
 };
