018a05349b53a0305519af399a6c562a50b230a3
fruitbox12 <dylanwong007@gmail.com>
2023-06-10 23:30:29 -0700

    Update TwitterVerifier.sol

--- implementation/twitter-verification/contracts/TwitterVerifier.sol
+++ implementation/twitter-verification/contracts/TwitterVerifier.sol
@@ -1,102 +1,111 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./lib/dev/functions/FunctionsClient.sol";
-// import "@chainlink/contracts/src/v0.8/dev/functions/FunctionsClient.sol"; // Once published
-import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
-import "./utils/AddressToString.sol";
+import {Functions, FunctionsClient} from "./lib/dev/functions/FunctionsClient.sol";
 
-/**
- * @title Functions Copns contract
- * @notice This contract is a demonstration of using Functions.
- * @notice NOT FOR PRODUCTION USE
- */
-contract TwitterVerifier is FunctionsClient, ConfirmedOwner {
-    using Functions for Functions.Request;
+contract TwitterVerifier is FunctionsClient {
+  using Functions for Functions.Request;
 
-    bytes32 public latestRequestId;
-    bytes public latestResponse;
-    bytes public latestError;
+  bytes32 public latestRequestId;
+  bytes public latestResponse;
+  bytes public latestError;
+  event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
 
-    event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
+  address public insurer;
+  address public immutable client;
 
-    /**
-     * @notice Executes once when a contract is created to initialize state variables
-     *
-     * @param oracle - The FunctionsOracle contract
-     */
-    constructor(
-        address oracle
-    ) FunctionsClient(oracle) ConfirmedOwner(msg.sender) {}
+  // Check if the contract active or end
+  bool public contractActive;
 
-    /**
-     * @notice Send a simple request
-     * @param source JavaScript source code
-     * @param secrets Encrypted secrets payload
-     * @param args List of arguments accessible from within the source code
-     * @param subscriptionId Billing ID
-     */
-    function executeRequest(
-        string calldata source,
-        bytes calldata secrets,
-        string[] calldata args,
-        uint64 subscriptionId,
-        uint32 gasLimit
-    ) public onlyOwner returns (bytes32) {
-        Functions.Request memory req;
-        req.initializeRequest(
-            Functions.Location.Inline,
-            Functions.CodeLanguage.JavaScript,
-            source
-        );
-        if (secrets.length > 0) req.addInlineSecrets(secrets);
+  // Check if the client has reached the goal
+  bool public goalReached;
 
-        // The following is how we would add some arguments provided in the contract
-        // to the request
-        // In this case, it is not necessary, since the msg.sender will always
-        // be the owner, and not the user
-        // - Turn the sender address into a string
-        // string memory sender = addressToString(msg.sender);
-        // - And add it to the args
-        // string[] memory newArgs = new string[](args.length + 1);
-        // for (uint256 i = 0; i < args.length; i++) {
-        // newArgs[i] = args[i];
-        // }
-        // newArgs[args.length] = sender;
+  // Staked amount by insurer
+  uint256 public stakedAmount;
 
-        // req.addArgs(newArgs);
-        req.addArgs(args);
+  // Goal to be reached
+  uint256 public goal;
 
-        bytes32 assignedReqID = sendRequest(
-            req,
-            subscriptionId,
-            gasLimit,
-            tx.gasprice
-        );
-        latestRequestId = assignedReqID;
-        return assignedReqID;
+  // Current progress towards the goal
+  uint256 public currentProgress;
+
+  constructor(address oracle, address _client, uint256 _goal) payable FunctionsClient(oracle) {
+    insurer = msg.sender;
+    client = _client;
+    goalReached = false;
+    contractActive = true;
+    goal = _goal;
+    currentProgress = 0;
+  }
+
+  /**
+   * @notice Stake ether in the contract
+   */
+  function stake() public payable {
+    require(msg.sender == insurer, "Only insurer can stake");
+    stakedAmount += msg.value;
+  }
+
+  /**
+   * @notice Send a simple request
+   * @param source JavaScript source code
+   * @param secrets Encrypted secrets payload
+   * @param args List of arguments accessible from within the source code
+   * @param subscriptionId Billing ID
+   */
+  function executeRequest(
+    string calldata source,
+    bytes calldata secrets,
+    string[] calldata args,
+    uint64 subscriptionId,
+    uint32 gasLimit
+  ) public returns (bytes32) {
+    require(contractActive, "Contract has ended");
+    Functions.Request memory req;
+    req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, source);
+    if (secrets.length > 0) {
+      req.addRemoteSecrets(secrets);
     }
+    if (args.length > 0) req.addArgs(args);
 
-    /**
-     * @notice Callback that is invoked once the DON has resolved the request or hit an error
-     *
-     * @param requestId The request ID, returned by sendRequest()
-     * @param response Aggregated response from the user code
-     * @param err Aggregated error from the user code or from the execution pipeline
-     * Either response or error parameter will be set, but never both
-     */
-    function fulfillRequest(
-        bytes32 requestId,
-        bytes memory response,
-        bytes memory err
-    ) internal override {
-        // revert('test');
-        latestResponse = response;
-        latestError = err;
-        emit OCRResponse(requestId, response, err);
-    }
+    bytes32 assignedReqID = sendRequest(req, subscriptionId, gasLimit);
+    latestRequestId = assignedReqID;
+    return assignedReqID;
+  }
 
-    function updateOracleAddress(address oracle) public onlyOwner {
-        setOracle(oracle);
+  /**
+   * @notice Callback that is invoked once the DON has resolved the request or hit an error
+   *
+   * @param requestId The request ID, returned by sendRequest()
+   * @param response Aggregated response from the user code
+   * @param err Aggregated error from the user code or from the execution pipeline
+   * Either response or error parameter will be set, but never both
+   */
+  function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+    latestResponse = response;
+    latestError = err;
+    emit OCRResponse(requestId, response, err);
+
+    currentProgress = uint256(bytes32(response));
+
+    // pay the client and shut down the contract
+    if (currentProgress >= goal) {
+      payoutContract();
     }
+  }
+
+  /**
+   * @dev Goal conditions have been met, do payout of total staked amount to client
+   */
+  function payoutContract() internal {
+    require(contractActive, "Contract has ended");
+    (bool sent /*bytes memory data*/, ) = client.call{value: stakedAmount}("");
+    contractActive = !sent;
+    goalReached = sent;
+  }
+
+  /**
+   * @dev Receive function so contract can receive ether when required
+   */
+  receive() external payable {}
 }
